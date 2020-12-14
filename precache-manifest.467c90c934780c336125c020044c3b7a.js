d1f4f9178a20adc8396452f3897add973984fa38
Guido Laures <guido@laures.de>
2020-12-14 17:08:08 +0100

    deploy

---
+++ precache-manifest.467c90c934780c336125c020044c3b7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82715a43098e63ee5172",
+    "url": "/resultserver-spa/css/app.af85209b.css"
+  },
+  {
+    "revision": "7907d2ea5e2e44e35106",
+    "url": "/resultserver-spa/css/chunk-vendors.c5b0137c.css"
+  },
+  {
+    "revision": "ff6cde1472117effdd9074832fd8a2a0",
+    "url": "/resultserver-spa/index.html"
+  },
+  {
+    "revision": "82715a43098e63ee5172",
+    "url": "/resultserver-spa/js/app.6cfab456.js"
+  },
+  {
+    "revision": "7907d2ea5e2e44e35106",
+    "url": "/resultserver-spa/js/chunk-vendors.afd514f5.js"
+  },
+  {
+    "revision": "31767dbb2887c8b511c3d72004281c74",
+    "url": "/resultserver-spa/manifest.json"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/resultserver-spa/precache-manifest.467c90c934780c336125c020044c3b7a.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "Sandkastenlige_Resultserver"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
